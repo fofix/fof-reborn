1d01ac299e7566d12e07fd531f76ef1b409cf604
Matthew Sitton <matthewsitton@gmail.com>
2015-10-18 19:56:19 -0500

    Rename tostring to tobytes for latest pillow changes.

--- src/Texture.py
+++ src/Texture.py
@@ -2,7 +2,7 @@
 # -*- coding: iso-8859-1 -*-                                        #
 #                                                                   #
 # Frets on Fire                                                     #
-# Copyright (C) 2006 Sami Kyöstilä                                  #
+# Copyright (C) 2006 Sami KyÃ¶stilÃ¤                                  #
 #                                                                   #
 # This program is free software; you can redistribute it and/or     #
 # modify it under the terms of the GNU General Public License       #
@@ -202,13 +202,13 @@ class Texture:
         """Load the texture from a PIL image"""
         image = image.transpose(Image.FLIP_TOP_BOTTOM)
         if image.mode == "RGBA":
-            string = image.tostring('raw', 'RGBA', 0, -1)
+            string = image.tobytes('raw', 'RGBA', 0, -1)
             self.loadRaw(image.size, string, GL_RGBA, 4)
         elif image.mode == "RGB":
-            string = image.tostring('raw', 'RGB', 0, -1)
+            string = image.tobytes('raw', 'RGB', 0, -1)
             self.loadRaw(image.size, string, GL_RGB, 3)
         elif image.mode == "L":
-            string = image.tostring('raw', 'L', 0, -1)
+            string = image.tobytes('raw', 'L', 0, -1)
             self.loadRaw(image.size, string, GL_LUMINANCE, 1)
         else:
             raise TextureException("Unsupported image mode '%s'" % image.mode)
@@ -248,7 +248,7 @@ class Texture:
             # appears to be using PIL to do the conversion.
             string = pygame.image.tostring(surface, "RGB")
             image = Image.fromstring("RGB", surface.get_size(), string).convert("L")
-            string = image.tostring('raw', 'L', 0, -1)
+            string = image.tobytes('raw', 'L', 0, -1)
             self.loadRaw(surface.get_size(), string, GL_LUMINANCE, GL_INTENSITY8)
         else:
             if alphaChannel:
@@ -267,7 +267,7 @@ class Texture:
             # appears to be using PIL to do the conversion.
             string = pygame.image.tostring(surface, "RGB")
             image = Image.fromstring("RGB", surface.get_size(), string).convert("L")
-            string = image.tostring('raw', 'L', 0, -1)
+            string = image.tobytes('raw', 'L', 0, -1)
             self.loadSubRaw(surface.get_size(), position, string, GL_INTENSITY8)
         else:
             if alphaChannel:
